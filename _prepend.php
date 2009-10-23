9fa695f5d52db1caeb58bb14f69af5689a2be0c7
Tomtom33 <Tomtom33@b35155ea-a742-4b26-8089-0891bb790021>
2009-10-23 08:09:53 +0000

    Added trunk / tags folders and reorganized this crap

---
+++ _prepend.php
@@ -0,0 +1,16 @@
+<?php
+# -- BEGIN LICENSE BLOCK ----------------------------------
+# This file is part of dcWikipedia, a plugin for Dotclear.
+# 
+# Copyright (c) 2009 Tomtom
+# http://blog.zenstyle.fr/
+# 
+# Licensed under the GPL version 2.0 license.
+# A copy of this license is available in LICENSE file or at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# -- END LICENSE BLOCK ------------------------------------
+
+$__autoload['dcWikipediaReader'] = dirname(__FILE__).'/inc/class.dc.wikipedia.reader.php';
+$__autoload['dcWikipediaParser'] = dirname(__FILE__).'/inc/class.dc.wikipedia.parser.php';
+
+?>
