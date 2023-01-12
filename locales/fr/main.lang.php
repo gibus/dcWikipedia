bb781e24caa28b2aaa4b902e457a6e65dbb8c84c
gibus <gibus@easter-eggs.com>
2023-01-12 10:40:57 +0100

    Add php localization file from po

---
+++ locales/fr/main.lang.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+l10n::$locales['Wikipedia'] = 'Wikipédia';
+l10n::$locales['Please, select a word or an expression'] = 'Sélectionnez un mot ou une expression s\'il vous plaît';
+l10n::$locales['dcWikipedia'] = 'dcWikipedia';
+l10n::$locales['Add lang flag in link'] = 'Ajouter la langue dans le lien généré';
+l10n::$locales['Add a Wikipedia link'] = 'Ajouter un lien Wikipédia';
+l10n::$locales['No suggestion found for : %s in %s'] = 'Aucune suggestion trouvée pour : %s en %s';
+l10n::$locales['Read more...'] = 'En lire plus…';
+l10n::$locales['No suggestion found for : %s'] = 'Aucune suggestion trouvée pour : %s';
