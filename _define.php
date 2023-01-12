ea47150f4fb1fa855519f0a0745ec08a567ced76
gibus <gibus@easter-eggs.com>
2023-01-12 10:27:36 +0100

    Use dc 2.24 permission constants

--- _define.php
+++ _define.php
@@ -21,7 +21,10 @@ $this->registerModule(
     '0.3.1',                                                     // Version
     [
         'requires'    => [['core', '2.24']],                     // Dependencies
-        'permissions' => 'usage,contentadmin',                   // Permissions
+        'permissions' => dcCore::app()->auth->makePermissions([
+            dcAuth::PERMISSION_USAGE,
+            dcAuth::PERMISSION_CONTENT_ADMIN,
+        ]),
         'type'        => 'plugin',                               // Type
         'priority'    => 666,                                    // Priority
         'support'     => 'https://github.com/gibus/dcWikipedia', // Support URL
