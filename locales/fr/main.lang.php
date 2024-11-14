672a8d97256b8458b13b2a48d31d18098ae5d668
gibus <gibus@easter-eggs.com>
2024-11-14 15:54:37 +0100

    Update translation php file to Dotclear 2.27

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -1,24 +1,21 @@
 <?php
 /**
- * @brief dcWikipedia, a plugin for Dotclear 2
- *
  * @package Dotclear
- * @subpackage Plugins
  *
- * @author Tomtom and Gibus
- *
- * @copyright Tomtom, Gibus gibus@sedrati.xyz
- * @copyright WTFLP Version 2 http://www.wtfpl.net/
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
  */
 #
 # DOT NOT MODIFY THIS FILE !
 #
 
-l10n::$locales['Wikipedia']                              = 'Wikipédia';
-l10n::$locales['Please, select a word or an expression'] = 'Sélectionnez un mot ou une expression s\'il vous plaît';
-l10n::$locales['dcWikipedia']                            = 'dcWikipedia';
-l10n::$locales['Add lang flag in link']                  = 'Ajouter la langue dans le lien généré';
-l10n::$locales['Add a Wikipedia link']                   = 'Ajouter un lien Wikipédia';
-l10n::$locales['No suggestion found for : %s in %s']     = 'Aucune suggestion trouvée pour : %s en %s';
-l10n::$locales['Read more...']                           = 'En lire plus…';
-l10n::$locales['No suggestion found for : %s']           = 'Aucune suggestion trouvée pour : %s';
+use Dotclear\Helper\L10n;
+
+L10n::$locales['Wikipedia'] = 'Wikipédia';
+L10n::$locales['Please, select a word or an expression'] = 'Sélectionnez un mot ou une expression s\'il vous plaît';
+L10n::$locales['dcWikipedia'] = 'dcWikipedia';
+L10n::$locales['Add lang flag in link'] = 'Ajouter la langue dans le lien généré';
+L10n::$locales['Add a Wikipedia link'] = 'Ajouter un lien Wikipédia';
+L10n::$locales['No suggestion found for : %s in %s'] = 'Aucune suggestion trouvée pour : %s en %s';
+L10n::$locales['Read more...'] = 'En lire plus…';
+L10n::$locales['No suggestion found for : %s'] = 'Aucune suggestion trouvée pour : %s';
