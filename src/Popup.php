7b342253d53af62a9db0abfa4bf7b24b3687e739
gibus <gibus@easter-eggs.com>
2024-11-18 13:00:42 +0100

    Use 2.27 core libs to output html forms

--- src/Popup.php
+++ src/Popup.php
@@ -10,58 +10,63 @@
  * @copyright Tomtom, Gibus gibus@sedrati.xyz
  * @copyright WTFLP Version 2 http://www.wtfpl.net/
  */
+declare(strict_types=1);
+
+namespace Dotclear\Plugin\dcWikipedia;
+
+use Dotclear\App;
+use Dotclear\Core\Backend\Combos;
+use Dotclear\Core\Backend\Page;
+use Dotclear\Core\Process;
+use Dotclear\Helper\Html\Form\Button;
+use Dotclear\Helper\Html\Form\Div;
+use Dotclear\Helper\Html\Form\Form;
+use Dotclear\Helper\Html\Form\Hidden;
+use Dotclear\Helper\Html\Form\Label;
+use Dotclear\Helper\Html\Form\Link;
+use Dotclear\Helper\Html\Form\Para;
+use Dotclear\Helper\Html\Form\Radio;
+use Dotclear\Helper\Html\Form\Select;
+use Dotclear\Helper\Html\Form\Text;
+use Dotclear\Helper\Html\Html;
+use Dotclear\Helper\L10n;
+
 $value = isset($_GET['value']) ? rawurldecode($_GET['value']) : '';
 $lang  = isset($_GET['lang']) ? rawurldecode($_GET['lang']) : '';
 
 $parser = dcWikipediaReader::quickParse('http://' . $lang . '.wikipedia.org/w/api.php?action=opensearch&format=xml&search=' . rawurlencode($value), DC_TPL_CACHE);
 
 $flag = 'no';
-$flag = dcCore::app()->blog->settings->dcwikipedia->dcwp_add_lang_flag ? 'yes' : 'no';
+$flag = App::blog()->settings->dcwikipedia->dcwp_add_lang_flag ? 'yes' : 'no';
 
-?>
+$head = My::jsLoad('popup') . My::cssLoad('style');
 
-<html>
-<head>
-    <title><?php echo __('dcWikipedia'); ?></title>
-    <?php echo dcPage::jsLoad(DC_ADMIN_URL . '?pf=dcWikipedia/js/popup.js'); ?>
-    <style type="text/css">@import '<?php echo DC_ADMIN_URL; ?>?pf=dcWikipedia/style.css';</style>
-  <script type="text/javascript">
-  //<![CDATA[
-  const dcWikipedia_option_langFlag = '<?php echo $flag; ?>';
-  //]]>
-  </script>
-</head>
+Page::openModule(My::name(), $head);
 
-<body>
-<h2><?php echo __('Add a Wikipedia link'); ?></h2>
+echo (new Text('h2', __('Add a Wikipedia link')))->render();
 
-<?php
+$lang_combo = Combos::getLangsCombo(App::blog()->getLangs(['order' => 'asc']), true);
+$all_langs  = L10n::getISOcodes(false, true);
 
-$rs         = dcCore::app()->blog->getLangs(['order' => 'asc']);
-$all_langs  = l10n::getISOcodes(false, true);
-$lang_combo = ['' => '', __('Most used') => [], __('Available') => l10n::getISOcodes(true, true)];
-while ($rs->fetch()) {
-    if (isset($all_langs[$rs->field('post_lang')])) {
-        $lang_combo[__('Most used')][$all_langs[$rs->field('post_lang')]] = $rs->field('post_lang');
-        unset($lang_combo[__('Available')][$all_langs[$rs->field('post_lang')]]);
-    } else {
-        $lang_combo[__('Most used')][$rs->field('post_lang')] = $rs->field('post_lang');
-    }
-}
-unset($rs);
+echo (new Form('dcwikipedia-lang-form'))
+    ->action(DC_ADMIN_URL . 'plugin.php')
+    ->method('get')
+    ->fields([
+        (new Hidden('p', 'dcWikipedia')),
+        (new Hidden('popup', '1')),
+        (new Hidden('value', $value)),
+        (new Para())->items([
+            (new Select('lang'))
+                ->label(new Label(__('Language:'), Label::INSIDE_LABEL_BEFORE))
+                ->items($lang_combo)
+                ->default($lang),
+        ]),
+        App::nonce()->formNonce(),
+    ])
+->render();
 
-echo
-'<form id="dcwikipedia-lang-form" action="' . DC_ADMIN_URL . 'plugin.php" method="get">' .
-form::hidden('p', 'dcWikipedia') .
-form::hidden('popup', '1') .
-form::hidden('value', $value) .
-'<p><label for="lang">' . __('Lang:') . '' .
-form::combo('lang', $lang_combo, $lang) .
-'</label></p>' .
-dcCore::app()->formNonce() .
-'</form>';
-
-if (count($parser->getItems()) == 0) {
+$items = $parser->getItems();
+if (!$items) {
     echo
     '<p>' . sprintf(
         __('No suggestion found for : %s in %s'),
@@ -69,26 +74,37 @@ if (count($parser->getItems()) == 0) {
         '<em>' . $all_langs[$lang] . '</em>'
     ) . '</p>';
 } else {
-    echo '<form id="dcwikipedia-insert-form" action="#" method="get">';
-    foreach ($parser->getItems() as $v => $k) {
-        echo
-        '<div class="dcwikipedia_item">' .
-        '<p class="wikipedia_value">' .
-        form::radio(['dcwikipedia_uri'], $k['uri']) .
-        $k['value'] . '&nbsp;<a href="' . $k['uri'] .
-        '" class="wikipedia_uri">(' . __('Read more...') . ')</a></p>' .
-        '<p class="wikipedia_desc">' . $k['desc'] . '</p>' .
-        '</div>' . "\n";
+    $choices = [];
+    foreach ($items as $v => $k) {
+        $choices[] = (new Div)->class('dcwikipedia_item')->items([
+            (new Para())->class('wikipedia_value')->items([
+                (new Radio('dcwikipedia_uri'))
+                    ->value($k['uri']),
+                (new Text(null, $k['value'] . '&nbsp;' . (new Link())->href($k['uri'])->class('wikipedia_uri')->text(__('Read more...'))->render())),
+                (new Para())->class('wikipedia_desc')->items([
+                    (new Text(null, $k['desc'])),
+                ]),
+            ]),
+        ]);
     }
-    echo
-    '<input type="hidden" name="dcwikipedia_value" value="' . $value . '" />' .
-    '<input type="hidden" name="dcwikipedia_lang" value="' . $lang . '" />' .
-    '</form>' .
-    '<p><a class="button" href="#" id="dcwikipedia-insert-cancel">' . __('cancel') . '</a> - ' .
-    '<strong><a class="button" href="#" id="dcwikipedia-insert-ok">' . __('insert') . '</a></strong></p>' . "\n";
+
+    echo (new Form('dcwikipedia-insert-form'))
+        ->method('get')
+        ->fields([
+            (new Para())->items([
+                ...$choices,
+            ]),
+            (new Hidden('dcwikipedia_value', $value)),
+            (new Hidden('dcwikipedia_lang', $lang)),
+            (new Hidden('dcwikipedia_langFlag', $flag)),
+            (new Button('dcwikipedia-insert-ok'))
+                ->class('submit')
+                ->value(__('Insert')),
+            (new Button('dcwikipedia-insert-cancel'))
+                ->class('submit')
+                ->value(__('Cancel')),
+        ])
+        ->render();
 }
 
-?>
-
-</body>
-</html>
+Page::closeModule();
