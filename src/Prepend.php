a0acb881a1da6cecdf57f0f0f9ac6102bd060880
gibus <gibus@easter-eggs.com>
2024-11-18 12:58:16 +0100

    Use require instead of Clearbricks::lib()->autoload to load php classes

--- src/Prepend.php
+++ src/Prepend.php
@@ -10,5 +10,5 @@
  * @copyright Tomtom, Gibus gibus@sedrati.xyz
  * @copyright WTFLP Version 2 http://www.wtfpl.net/
  */
-Clearbricks::lib()->autoload(['dcWikipediaReader' => dirname(__FILE__) . '/inc/class.dc.wikipedia.reader.php']);
-Clearbricks::lib()->autoload(['dcWikipediaParser' => dirname(__FILE__) . '/inc/class.dc.wikipedia.parser.php']);
+require dirname(__FILE__) . '/inc/class.dc.wikipedia.reader.php';
+require dirname(__FILE__) . '/inc/class.dc.wikipedia.parser.php';
